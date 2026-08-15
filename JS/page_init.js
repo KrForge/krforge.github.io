796dc9f3fae5800d7aea34f1550b4c33d9408294
KrForge <kloosgage8@gmail.com>
2026-08-15 00:41:33 -0400

    Adding warning message

--- JS/page_init.js
+++ JS/page_init.js
@@ -7,3 +7,5 @@ function Topbar() {
 }
 
 Topbar();
+
+alert("My site is currently undergoing a rework to be easier to modify in the future. If you encounter any bugs while on this site, the upcoming, new site will most likely address them.\n\n- KrForge");
